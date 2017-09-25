6f45b7c971d0245997ca0d91f6f20f7bb95dc66b
Pushpraj Ruhal <pur@sitecore.net>
2017-09-25 14:10:27 +0800

    Fix Template Builder tab key navigation issue

---
+++ src/Sitecore.Support.94854/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94854")]
+[assembly: AssemblyProduct("Sitecore.Support.94854")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Fix Template Builder tab key navigation issue")]
+
